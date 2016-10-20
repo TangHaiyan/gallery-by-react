e3cb1bd90c2d838c393c8c51cf3b4265e9d49711
Tanghaiyan <584536319@qq.com>
2016-10-20 11:48:54 +0800

    json-loader

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,30 +2,40 @@ require('normalize.css/normalize.css');
 require('styles/App.scss');
 
 import React from 'react';
-import ReactDOM from 'react-DOM';
+//import ReactDOM from 'react-DOM';
+
+//获取图片相关的数据
+let imageDatas = require('../data/imageDatas.json');
+
+//利用自执行函数，将图片名信息转成图片URL路径信息
+
+imageDatas = ((imageDatasArr) => {
+  for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+    let singleImageData = imageDatasArr[i];
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+    imageDatasArr[i] = singleImageData;
+  }
+  return imageDatasArr;
+})(imageDatas);
 
 
-let imageDatas=require('json!../data/imageDatas.json');
-let imageData1=imageDatas[1];
-//let arr=[1,2,3]
-
-//let yeomanImage = require('../images/yeoman.png');
-
-class AppComponent extends React.Component {
+class GalleryByReactApp extends React.Component {
   render() {
     return (
-      // <div className="index">
-      //   <img src={yeomanImage} alt="Yeoman Generator" />
-      //   <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      // </div>
-      //<span>nihao</span>
-      <span> {imageData1.desc} </span>
-      
+      <section className="stage" >
+        <section className="img-sec">
+        </section>
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
+
+           
     );
   }
 }
 
-AppComponent.defaultProps = {
+GalleryByReactApp.defaultProps = {
 };
 
-export default AppComponent;
+export default GalleryByReactApp;
