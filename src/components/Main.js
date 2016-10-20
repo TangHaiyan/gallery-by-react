5d497e20add9cd795be62ac324d3b552e4770304
Tanghaiyan <584536319@qq.com>
2016-10-20 10:49:52 +0800

    json-loader

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,14 @@ require('normalize.css/normalize.css');
 require('styles/App.scss');
 
 import React from 'react';
+import ReactDOM from 'react-DOM';
 
-// let yeomanImage = require('../images/yeoman.png');
+
+let imageDatas=require('json!../data/imageDatas.json');
+let imageData1=imageDatas[1];
+//let arr=[1,2,3]
+
+//let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
@@ -12,7 +18,9 @@ class AppComponent extends React.Component {
       //   <img src={yeomanImage} alt="Yeoman Generator" />
       //   <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       // </div>
-      <span>nihao</span>
+      //<span>nihao</span>
+      <span> {imageData1.desc} </span>
+      
     );
   }
 }
